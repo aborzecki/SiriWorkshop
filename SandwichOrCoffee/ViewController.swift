47fa47793267f08550ce964fc3f3ec34627c39b5
Adam Borzecki <adam@symbilityintersect.com>
2019-03-19 14:03:29 -0400

    Some visual tweaks

--- SandwichOrCoffee/ViewController.swift
+++ SandwichOrCoffee/ViewController.swift
@@ -12,9 +12,17 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        navigationController?.isNavigationBarHidden = false
+    }
 }
-
