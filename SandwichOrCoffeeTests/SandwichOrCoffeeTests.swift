45cbed3a1c663f5f0d14e330e9a267c30e656b89
Adam Borzecki <adam@symbilityintersect.com>
2019-03-19 13:36:47 -0400

    Initial Commit

---
+++ SandwichOrCoffeeTests/SandwichOrCoffeeTests.swift
@@ -0,0 +1,34 @@
+//
+//  SandwichOrCoffeeTests.swift
+//  SandwichOrCoffeeTests
+//
+//  Created by Adam Borzecki on 3/19/19.
+//  Copyright © 2019 SiriWorkshop. All rights reserved.
+//
+
+import XCTest
+@testable import SandwichOrCoffee
+
+class SandwichOrCoffeeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
